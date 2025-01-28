c1c0b01b2591597c471a8aed63754a22f0491be4
Arturo <arturotinajas@gmail.com>
2025-01-28 19:25:35 +0100

    Chngs

--- js/script.js
+++ js/script.js
@@ -1,2 +1,51 @@
-// Add some interactivity to the website
-console.log("Hello, world!");
+// script.js
+
+const songs = [
+  {
+    title: "Song 1",
+    author: "Artist 1",
+    src: "song1.mp3",
+    img: "cover1.jpg",
+  },
+  {
+    title: "Song 2",
+    author: "Artist 2",
+    src: "song2.mp3",
+    img: "cover2.jpg",
+  },
+];
+
+let currentSongIndex = 0;
+const audio = new Audio(songs[currentSongIndex].src);
+
+document.getElementById("playButton").addEventListener("click", playPause);
+document.getElementById("nextButton").addEventListener("click", nextSong);
+document.getElementById("prevButton").addEventListener("click", prevSong);
+audio.addEventListener("timeupdate", updateProgressBar);
+
+function playPause() {
+  // Add pause button implementation
+}
+
+function nextSong() {
+  // Add next button implementation
+}
+
+function prevSong() {
+  // Add previous button implementation
+}
+
+function loadSong(index) {
+  // Add load song implementation
+}
+
+function updateProgressBar() {
+  // Handle when progress bar is updated
+}
+
+document.getElementById("progressBar").addEventListener("input", function () {
+  audio.currentTime = (this.value / 100) * audio.duration;
+});
+
+// Initial load
+loadSong(currentSongIndex);
